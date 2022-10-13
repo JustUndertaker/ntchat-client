08850ea5b042a9ba7ee3d652a333279b900e9fe4
justundertaker <806792561@qq.com>
2022-10-14 03:30:01 +0800

    :bug:调度器阻塞

--- ntchat_client/__init__.py
+++ ntchat_client/__init__.py
@@ -10,31 +10,32 @@ from ntchat_client.client import Client
 from ntchat_client.config import Config, Env
 from ntchat_client.log import default_filter, logger
 
-_manager: Client = None
-"""全局管理器"""
+
+def tick():
+    logger.trace("心跳tick事件...")
 
 
 def init():
     """初始化"""
-    global _manager
-    if not _manager:
-        env = Env()
-        config = Config(_common_config=env.dict())
-        default_filter.level = config.log_level
-        logger.info(f"Current <y><b>Env: {env.environment}</b></y>")
-        logger.debug(f"Loaded <y><b>Config</b></y>: {str(config.dict())}")
-        logger.info("初始化ntchat_client...")
-        Path(config.cache_path).mkdir(parents=True, exist_ok=True)
-        _manager = Client(config)
-        _manager.init()
-        logger.debug("初始化定时器...")
-        scheduler = BlockingScheduler()
-        job = partial(scheduler_job, config)
-        scheduler.add_job(func=job, trigger="cron", hour=0, minute=0)
-        logger.debug("定时器启动成功...")
-        try:
-            scheduler.start()
-        except (KeyboardInterrupt, SystemExit):
-            scheduler.shutdown()
-            ntchat.exit_()
-            sys.exit()
+
+    env = Env()
+    config = Config(_common_config=env.dict())
+    default_filter.level = config.log_level
+    logger.info(f"Current <y><b>Env: {env.environment}</b></y>")
+    logger.debug(f"Loaded <y><b>Config</b></y>: {str(config.dict())}")
+    logger.info("初始化ntchat_client...")
+    Path(config.cache_path).mkdir(parents=True, exist_ok=True)
+    ntchat_client = Client(config)
+    ntchat_client.init()
+    logger.debug("初始化定时器...")
+    scheduler = BlockingScheduler()
+    job = partial(scheduler_job, config)
+    scheduler.add_job(func=job, trigger="cron", hour=0, minute=0)
+    scheduler.add_job(func=tick, trigger="interval", seconds=3)
+    logger.debug("定时器启动成功...")
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown(wait=False)
+        ntchat.exit_()
+        sys.exit()
