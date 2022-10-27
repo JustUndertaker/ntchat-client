f76c711b1dd23fa2f26851053f5b47bba14d5562
justundertaker <806792561@qq.com>
2022-10-27 14:16:37 +0800

    :fire:删除多余引用

--- ntchat_client/__init__.py
+++ ntchat_client/__init__.py
@@ -9,7 +9,6 @@ from ntchat_client.utils import notify
 from ntchat_client.websocket import websocket_init, websocket_shutdown
 from ntchat_client.wechat import (
     get_wechat_client,
-    scheduler_image_job,
     send_event_loop,
     wechat_init,
     wechat_shutdown,
